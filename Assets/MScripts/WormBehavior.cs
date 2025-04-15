41556e85c4e6e4656bc4748a47a14b65cdb998e3
maklsmit <156136111+maklsmit@users.noreply.github.com>
2025-04-15 12:34:33 -0400

    Better Worm Particles

--- Assets/MScripts/WormBehavior.cs
+++ Assets/MScripts/WormBehavior.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class WormBehavior : MonoBehaviour
 {
+    [SerializeField] ParticleSystem particles;
+    bool canPlay = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,26 @@ public class WormBehavior : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // IEnumerator Particles(){
+    //     // while(true){
+    //         // yield return new WaitForSeconds(2);
+    //         particles.SetActive(true);
+    //         yield return new WaitForSeconds(3);
+    //         particles.SetActive(false);
+    //         // yield return new WaitForSeconds(6);
+    //     // }
+    // }
+
+    public void PlayParticles(){
+        // StartCoroutine("Particles");
+        if(canPlay){
+            particles.Play();
+            canPlay = false;
+        }
+        else{
+            canPlay = true;
+        }
     }
 }
