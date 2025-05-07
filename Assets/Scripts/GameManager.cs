45a0c138e004f73bf667b76438348aafdf5dfde2
MyBestMyself <Rileybucket508@gmail.com>
2025-05-07 18:17:05 -0400

    game over screen, win screen, and music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -27,6 +28,10 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(sceneName: "howto");
     }
 
+    public void Retry()
+    {
+        SceneManager.LoadScene(sceneName: "level" + currentLevel);
+    }
     public void QuitGame()
     {
         Application.Quit();
